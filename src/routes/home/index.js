9d5d9238e8b29cda2be28ab4ac93806c1566ed0f
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2020-06-19 11:27:09 -0400

    check for load

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,6 +4,11 @@ import ArticleList from 'components/ArticleList';
 
 const Home = (props) => {
   const [data, isLoading] = usePrerenderData(props);
+
+  if (isLoading || !data || !data.data) {
+    return (<div class="pa3 pa4-ns" />);
+  }
+
   return (
     <div class="pa3 pa4-ns">
       { !isLoading && data && data.data && <ArticleList items={data.data.edges} /> }
