69404d49ee17a8d406289644e50f3ca6da6ebcc0
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2020-06-18 23:37:01 -0400

    remove loader

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,13 +1,11 @@
 import { h } from 'preact';
 import { usePrerenderData } from '@preact/prerender-data-provider';
-import Loader from '../../components/Loader';
-import ArticleList from '../../components/ArticleList';
+import ArticleList from 'components/ArticleList';
 
 const Home = (props) => {
   const [data, isLoading] = usePrerenderData(props);
   return (
     <div class="pa3 pa4-ns">
-      { isLoading && <Loader /> }
       { !isLoading && data && data.data && <ArticleList items={data.data.edges} /> }
     </div>
   );
