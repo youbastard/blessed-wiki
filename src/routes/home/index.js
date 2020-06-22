18954435e93237d3dce7d69db4708eea75feb89b
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2020-06-22 12:57:49 -0400

    remove body div element thingy

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,20 +1,11 @@
-import { useState, useEffect } from 'preact/hooks';
 import { usePrerenderData } from '@preact/prerender-data-provider';
 import ArticleList from 'components/ArticleList';
 
 const Home = (props) => {
   const [data, isLoading, error] = usePrerenderData(props);
-  // const [state, setState] = useState({ items: [] });
 
   if (isLoading) return <h1>Loading...</h1>;
   if (error) return <p>Error: {error}</p>;
-
-  // useEffect(() => {
-  //   if (data && data.articles) {
-  //     const items = data.articles.edges;
-  //     setState((s) => ({ ...s, items }));
-  //   }
-  // }, [data, isLoading]);
 
   return (
     <div>
