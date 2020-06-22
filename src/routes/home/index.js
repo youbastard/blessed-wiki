50b311d510f5574a7fabf124bc5dc80c60a79aed
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2020-06-22 12:53:41 -0400

    crggg

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,19 +3,22 @@ import { usePrerenderData } from '@preact/prerender-data-provider';
 import ArticleList from 'components/ArticleList';
 
 const Home = (props) => {
-  const [data, isLoading] = usePrerenderData(props);
-  const [state, setState] = useState({ items: [] });
+  const [data, isLoading, error] = usePrerenderData(props);
+  // const [state, setState] = useState({ items: [] });
 
-  useEffect(() => {
-    if (data && data.articles) {
-      const items = data.articles.edges;
-      setState((s) => ({ ...s, items }));
-    }
-  }, [data, isLoading]);
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <p>Error: {error}</p>;
+
+  // useEffect(() => {
+  //   if (data && data.articles) {
+  //     const items = data.articles.edges;
+  //     setState((s) => ({ ...s, items }));
+  //   }
+  // }, [data, isLoading]);
 
   return (
     <div>
-      { !isLoading && data && data.articles && <ArticleList items={state.items} /> }
+      { !isLoading && data && data.articles && <ArticleList items={data.articles.edges} /> }
     </div>
   );
 };
